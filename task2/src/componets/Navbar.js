7d631c300afa2ede22bb9fc6f8d9c7220996bb58
nihal1410 <98893038+nihal1410@users.noreply.github.com>
2022-07-30 23:06:45 +0530

    Add files via upload

---
+++ task2/src/componets/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+export default function nav(){
+    return(
+        <div >
+            <ul className="navname ">
+                <li style={{marginLeft:100}}>
+                    ADG Task-2
+                </li>
+            </ul>
+        </div>
+    )
+}
